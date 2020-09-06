b2511f85c2751dc7870bea9a4369f04ed79eeb31
Anna Martins <annalarissa.martins@hotmail.com>
2020-09-06 00:21:05 -0300

    Ajustando Steps

--- src/test/java/StepDefinitions/BuscaSteps.java
+++ src/test/java/StepDefinitions/BuscaSteps.java
@@ -1,33 +1,38 @@
 package StepDefinitions;
 
+import Helper.Helper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import Utils.driver;
 
 public class BuscaSteps {
+	
+	Helper helper;
+	
 	@Given("estou na pagina inicial")
 	public void estou_na_pagina_inicial() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		helper = new Helper(driver.GetDriver());
+		helper.AcessarSite("https://site.getnet.com.br/");
 	}
 
 	@When("clico no icone de pesquisa")
 	public void clico_no_icone_de_pesquisa() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		helper.ClicarIconePesquisa();
 	}
 
-	@When("Digitar {string} e clicar em procurar")
-	public void digitar_e_clicar_em_procurar(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	@When("digito {string} e clicar em procurar")
+	public void digito_e_clicar_em_procurar(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("Sistema apresenta os resultados")
-	public void sistema_apresenta_os_resultados() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	@When("o sistema apresenta os resultados")
+	public void o_sistema_apresenta_os_resultados() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
 	}
+
 
 	@When("clico no link {string}")
 	public void clico_no_link(String string) {
--- src/test/resources/Features/Busca.feature
+++ src/test/resources/Features/Busca.feature
@@ -3,8 +3,8 @@ Feature: Verificar onde posso realizar saques com o meu Cartão SuperGet?
   Scenario Outline: Como me informar onde posso realizar saques com o meu Cartão SuperGet?
     Given estou na pagina inicial
     When clico no icone de pesquisa
-    And Digitar '<campoBusca>' e clicar em procurar
-    When Sistema apresenta os resultados 
+    And digito '<campoBusca>' e clicar em procurar
+    When o sistema apresenta os resultados 
     And clico no link '<linkDuvida>'
     Then Verifico se o titulo da modal apresenta a frase '<linkDuvida>'
 
