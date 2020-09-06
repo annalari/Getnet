571466d3a6b4567173a6e31fe98c9be6d9e55fbc
Anna Martins <annalarissa.martins@hotmail.com>
2020-09-05 22:53:54 -0300

    adicionando steps

--- src/test/java/StepDefinitions/BuscaSteps.java
+++ src/test/java/StepDefinitions/BuscaSteps.java
@@ -1,5 +1,44 @@
 package StepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class BuscaSteps {
+	@Given("estou na pagina inicial")
+	public void estou_na_pagina_inicial() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clico no icone de pesquisa")
+	public void clico_no_icone_de_pesquisa() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Digitar {string} e clicar em procurar")
+	public void digitar_e_clicar_em_procurar(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Sistema apresenta os resultados")
+	public void sistema_apresenta_os_resultados() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clico no link {string}")
+	public void clico_no_link(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Verifico se o titulo da modal apresenta a frase {string}")
+	public void verifico_se_o_titulo_da_modal_apresenta_a_frase(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
 
 }
